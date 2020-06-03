7a92e0cd1ad51040b527e6a4aff853f503d98fe6
lhphan <phan1711999@gmail.com>
2020-06-03 21:13:52 +0700

    commit on 'create'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Auth; 
+use Exception;
 
 class Handler extends ExceptionHandler
 {
@@ -51,5 +54,25 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return redirect()->guest('/login/admin');
+        }
+        if ($request->is('giangvien') || $request->is('giangvien/*')) {
+            return redirect()->guest('/login/giangvien');
+        }
+        return redirect()->guest(route('login'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::view('/', 'welcome');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm')->name('login.admin');
+Route::get('/login/giangvien', 'Auth\LoginController@showWriterLoginForm')->name('login.giangvien');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm')->name('register.admin');
+Route::get('/register/giangvien', 'Auth\RegisterController@showWriterRegisterForm')->name('register.giangvien');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/giangvien', 'Auth\LoginController@writerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin')->name('register.admin');
+Route::post('/register/giangvien', 'Auth\RegisterController@createWriter')->name('register.giangvien');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::group(['middleware' => 'auth:admin'], function () {
+    Route::view('/admin', 'admin');
+});
+
+Route::group(['middleware' => 'auth:giangvien'], function () {
+    Route::view('/giangvien', 'giangvien');
+});
